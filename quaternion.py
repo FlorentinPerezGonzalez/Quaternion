6f4c46943f9f1c332009370e7c09b14bf332c4a4
alu0101100654 <alu0101100654@ull.edu.es>
2021-01-11 16:16:26 +0000

    Inicio

---
+++ quaternion.py
@@ -0,0 +1,73 @@
+import sys
+import numpy as np
+import quaternion
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from math import *
+
+# Dibujado
+def ramal(I,prev=[],base=0):
+    # Convierte el robot a una secuencia de puntos para representar
+    O = []
+    if I:
+        if isinstance(I[0][0],list):
+            for j in range(len(I[0])):
+                O.extend(ramal(I[0][j], prev, base or j < len(I[0])-1))
+        else:
+            O = [I[0]]
+            O.extend(ramal(I[1:],I[0],base))
+            if base:
+                O.append(prev)
+    return O
+
+def muestra_robot(O,ef=[]):
+  # Pinta en 3D
+  OR = ramal(O)
+  OT = np.array(OR).T
+  fig = plt.figure()
+  ax = fig.add_subplot(111, projection='3d')
+  # Bounding box cúbico para simular el ratio de aspecto correcto
+  max_range = np.array([OT[0].max()-OT[0].min()
+                       ,OT[1].max()-OT[1].min()
+                       ,OT[2].max()-OT[2].min()
+                       ]).max()
+  Xb = (0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][0].flatten()
+     + 0.5*(OT[0].max()+OT[0].min()))
+  Yb = (0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][1].flatten()
+     + 0.5*(OT[1].max()+OT[1].min()))
+  Zb = (0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][2].flatten()
+     + 0.5*(OT[2].max()+OT[2].min()))
+  for xb, yb, zb in zip(Xb, Yb, Zb):
+     ax.plot([xb], [yb], [zb], 'w')
+  ax.plot3D(OT[0],OT[1],OT[2],marker='s')
+  ax.plot3D([0],[0],[0],marker='o',color='k',ms=10)
+  if not ef:
+    ef = OR[-1]
+  ax.plot3D([ef[0]],[ef[1]],[ef[2]],marker='s',color='r')
+  ax.set_xlabel('X')
+  ax.set_ylabel('Y')
+  ax.set_zlabel('Z')
+  plt.show()
+  return
+
+def arbol_origenes(O,base=0,sufijo=''):
+  # Da formato a los origenes de coordenadas para mostrarlos por pantalla
+  if isinstance(O[0],list):
+    for i in range(len(O)):
+      if isinstance(O[i][0],list):
+        for j in range(len(O[i])):
+          arbol_origenes(O[i][j],i+base,sufijo+str(j+1))
+      else:
+        print('(O'+str(i+base)+sufijo+')0\t= '+str([round(j,3) for j in O[i]]))
+  else:
+    print('(O'+str(base)+sufijo+')0\t= '+str([round(j,3) for j in O]))
+
+def muestra_origenes(O,final=0):
+  # Muestra los orígenes de coordenadas para cada articulación
+  print('Orígenes de coordenadas:')
+  arbol_origenes(O)
+  if final:
+    print('E.Final = '+str([round(j,3) for j in final]))
+
+# Fin de dibujado
+
